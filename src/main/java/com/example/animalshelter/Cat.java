01b49c71d7740ecfdad1bcce5976ff047ef915ea
przemysław.idczak <przemyslaw.i2077@gmail.com>
2022-11-13 19:42:19 +0100

    Animal Shelter v1 test2

--- src/main/java/com/example/animalshelter/Cat.java
+++ src/main/java/com/example/animalshelter/Cat.java
@@ -13,7 +13,7 @@ import javax.persistence.Enumerated;
 @Setter
 @NoArgsConstructor
 public class Cat extends Animal{
-
+// cat
     @Enumerated(EnumType.STRING)
     private AnimalStatistics.CatRace race;
 
